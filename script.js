3f4b5e5ade6cfc27e8361d0ace8c940138d34d6b
Mitsiky <callistalarrivee@gmail.com>
2022-11-09 23:33:21 +0100

    Fin de la structure et début du squelette des pages annexes

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-var sidenav = document.getElementById("mySidenav");
-var openBtn = document.getElementById("openBtn");
-var closeBtn = document.getElementById("closeBtn");
-
-openBtn.onclick = openNav;
-closeBtn.onclick = closeNav;
-
-/* Set the width of the side navigation to 250px */
-function openNav() {
-    sidenav.classList.add("active");
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    sidenav.classList.remove("active");
-}
-
 /* Dropdown menu */
 $('.sub-menu ul').hide();
 $(".sub-menu a").click(function() {
